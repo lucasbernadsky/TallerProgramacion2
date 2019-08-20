7b200ef802c153cdef87e3f37b3c4df576005c0a
Lucas Bernadsky <lucasbernadsky@hotmail.com>
2019-08-20 18:55:45 -0300

    casi tp terminado

--- TP2/Clase 1/TP-01-apareo/apareo/apareo.js
+++ TP2/Clase 1/TP-01-apareo/apareo/apareo.js
@@ -75,8 +75,8 @@ function transformarStringEnArrayDeNumeros(str, separador) {
  * @returns {string} el nuevo string
  */
 function transformarArrayDeNumerosAUnSoloString(arr, separador) {
-  var str = "";
-  for (var i = 0; i < arr.length; i++) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
     if (!isNaN(arr[i])) {
       if (i === arr.length - 1) {
         str += arr[i].toString();
@@ -98,33 +98,55 @@ function transformarArrayDeNumerosAUnSoloString(arr, separador) {
  * @returns {number[]} un nuevo array de números ordenados
  */
 function combinarDosArrays(arrA, arrB) {
-  var arrCombinado = arrA.concat(arrB);
+  let arrCombinado = arrA.concat(arrB);
   arrCombinado = arrCombinado.sort(comparar);
 
-  return arrCombinado.toString();
+  return arrCombinado;
 }
 function comparar(a, b) {
   return a - b;
 }
-console.log(
+/**console.log(
   combinarDosArrays(
     [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, 22, 2, 34342, 19],
     [1241, 14]
   )
-);
+); */
 
 /**
  * toma un array de muchos arrays de números ordenados y los combina en uno solo, aún ordenado
  * @param {number[][]} arrs el array de arrays de números que quiero combinar
  * @returns {number[]} el nuevo array de números ordenados
  */
-function combinarNArrays(arrs) {}
+function combinarNArrays(arrs) {
+  let arrayNuevo = [].concat(...arrs).sort(comparar);
+  console.log(arrayNuevo);
+  return arrayNuevo;
+}
+
+combinarNArrays([
+  3,
+  2,
+  67,
+  1,
+  0,
+  [2, 31, 6],
+  [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]
+]);
 
 /**
  * devuelve el indice de la posicion con el menor valor de un array.
  * @param {number[]} valores el array de numeros
  * @returns {number} el indice del menor valor
  */
-function findMin(valores) {}
+function findMin(valores) {
+  let minimo = Math.min.apply(0, valores);
+  let index = valores.indexOf(minimo);
+  console.log("El minimo es " + minimo);
+  console.log("Esta en la posicion " + index);
+  return index;
+}
+
+findMin([4, 523, 2342, 2]);
 
 function actualizarIndicesYValores(indices, valores, arrs, min) {}
