e4da686fe4b0dffeef11d3b53394031b660c9322
Lucas Bernadsky <lucas.bernadsky@gmail.com>
2019-09-03 23:41:41 -0300

    request puerto x

--- Tema 4 - Servidor Web/Repositorio.js
+++ Tema 4 - Servidor Web/Repositorio.js
@@ -1,3 +1,3 @@
-const estudiantes = [
-  { nombre: "Lucas", apellido: "Bernadsky", DNI: 40935913, edad: 21 }
-];
+const estudiantes = [];
+
+module.exports = { estudiantes };
--- Tema 4 - Servidor Web/test.js
+++ Tema 4 - Servidor Web/test.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-const puerto = 9443;
+const puerto = 8080;
+
 app.listen(puerto, () => {
   console.log(`servidor inicializado en puerto  ${puerto} `);
+});
+
+let contador = 1;
+
+app.get("/api/mensajes", (req, res) => {
+  console.log("request " + contador++ + " recibido"); // acá debería obtener todos los recursos de tipo ‘mensaje’
+  res.json({ msg: "Hola mundo!" });
 });
